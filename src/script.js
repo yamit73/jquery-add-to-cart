593aa9d28dbca8b9fc8d132bc029fde5b3669e3e
Amit Yadav <amityadav@cedcoss.com>
2022-02-19 13:20:47 +0530

    initial commit

---
+++ src/script.js
@@ -0,0 +1,1 @@
+var products = [{"id":101,"name":"Basket Ball","image":"basketball.png","price":150},{"id":102,"name":"Football","image":"football.png","price":120},{"id":103,"name":"Soccer","image":"soccer.png","price":110},{"id":104,"name":"Table Tennis","image":"table-tennis.png","price":130},{"id":105,"name":"Tennis","image":"tennis.png","price":100}];
